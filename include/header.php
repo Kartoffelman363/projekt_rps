51c9206bfdf4e04445d2be6815808617ae75d8c6
SovicLeon <sovicleon@gmail.com>
2023-03-20 18:00:01 +0100

    Added template

---
+++ include/header.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ToDoList">
+    <meta name="keywords" content="ToDoList">
+    <meta name="author" content="ekipa">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <title>ToDoList</title>
+</head>
+<body>
+    <nav>
+        
+    </nav>
